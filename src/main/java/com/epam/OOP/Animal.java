433ddcfd9285eeaf1f8b8e33df1e68b4262ffab8
muhammadiyevas <muhammadiyevas862@gmail.com>
2023-06-14 21:45:21 +0500

    1-tasks

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -1,5 +1,19 @@
 package com.epam.OOP;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription(){
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + " paws and "
+                + (hasFur ? "fur." : "no fur.");
+    }
 
 }
